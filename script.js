acaa53ac18c4a505f73d4e39c16dad381d637226
nhyiraanarfi7-prog <nhyiraanarfi7@gmail.com>
2026-08-15 23:11:13 +0000

    Update script.js

--- script.js
+++ script.js
@@ -362,8 +362,8 @@ document.addEventListener("DOMContentLoaded", () => {
         try {
 
             const response = await fetch(
-                "http://127.0.0.1:5000/chat",
-                {
+                ,"https://anthropology-display-word-architectural.trycloudflare.com/chat",
+                
                     method: "POST",
 
                     headers: {
